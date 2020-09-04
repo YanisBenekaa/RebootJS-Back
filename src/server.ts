6a85d2e7e8c919283b227947e9d1cf2fc777aba3
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-04 12:35:13 +0200

    Updating configuration

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express, { Request, Response, ErrorRequestHandler } from 'express';
+import express, { Request, Response, ErrorRequestHandler, request } from 'express';
 import morgan from "morgan";
 import helmet from "helmet";
 import { configuration, IConfig } from "./config";
+import { connect } from './database';
+import { Profile } from './models/profiles';
 
 export function createExpressApp(config: IConfig): express.Express {
   const { express_debug } = config;
@@ -19,10 +21,20 @@ export function createExpressApp(config: IConfig): express.Express {
 
   app.get('/', (req: Request, res: Response) => { res.send('This is the boilerplate for Flint Messenger app') });
 
+  app.post('/profile', (req: Request, res: Response) => {
+    const {email, firstname, lastname } = req.body;
+
+    const newProfile = new Profile({email: email, firstname: firstname, lastname: lastname})
+    newProfile.save();
+    res.send('Utilisateur créé');
+  })
+
   return app;
 }
 
 const config = configuration();
 const { PORT } = config;
 const app = createExpressApp(config);
-app.listen(PORT, () => console.log(`Flint messenger listening at ${PORT}`));
+connect(config).then(
+  () =>  { app.listen(PORT, () => console.log(`Flint messenger listening at ${PORT}`)) }
+);
