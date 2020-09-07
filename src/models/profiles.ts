afe0f5e8409c3ce6b3eb352732e80f31c1e75ca7
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-07 15:16:49 +0200

    Add auth with passport

--- src/models/profiles.ts
+++ src/models/profiles.ts
@@ -1,16 +1,35 @@
 import { Document, Schema, model, Model } from "mongoose";
+import { SHA256 } from "crypto-js";
 
 export interface IProfile extends Document {
-    email: string;
-    lastname: string;
-    firstname: string;
+  email: string;
+  lastname: string;
+  firstname: string;
+  getFullname: () => string;
+  setPassword: (password: string) => void;
+  verifyPassword: (password: string) => boolean;
 }
 
 const profileSchema = new Schema({
-    email: { type: String, required: true, unique: true},
-    firstname: { type: String, required: true},
-    lastname: { type: String, required: true}
+  email: { type: String, required: true, unique: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-export const Profile = model<IProfile, Model<IProfile>>("profile", profileSchema);
+profileSchema.methods.getFullname = function () {
+  return `${this.firstname} ${this.lastname}`;
+};
 
+profileSchema.methods.setPassword = function (password: string) {
+  this.password = SHA256(password).toString();
+};
+
+profileSchema.methods.verifyPassword = function (password: string) {
+  return this.password === SHA256(password).toString();
+};
+
+export const Profile = model<IProfile, Model<IProfile>>(
+  "profile",
+  profileSchema
+);
