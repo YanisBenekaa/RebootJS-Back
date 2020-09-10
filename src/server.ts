21f66c7b2369b65812a5c419d86458a4477cfa53
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-10 17:20:51 +0200

    Add credentials and origin to cors

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ export function createExpressApp(config: IConfig): express.Express {
   app.use(morgan("combined"));
   app.use(helmet());
   app.use(express.json());
-  app.use(cors());
+  app.use(cors({credentials: true, origin: true}));
 
   app.use(
     session({
