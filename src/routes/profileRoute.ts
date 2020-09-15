08456b0ab4251857b6fca92b6f1c93d7bfa253ef
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-15 09:45:44 +0200

    Fix wrong usage of request in Delete route

--- src/routes/profileRoute.ts
+++ src/routes/profileRoute.ts
@@ -1,4 +1,4 @@
-import { request, Request, Response, Router } from "express";
+import { Request, Response, Router } from "express";
 import { IProfile, Profile } from "../models/profiles";
 import { authenticationRequired } from "../middlewares/authenticationRequired";
 import {
@@ -9,49 +9,35 @@ import {
 
 const router = Router();
 
-router.get(
-  "/me",
-  authenticationRequired,
-  (request: Request, response: Response) => {
-    if (!request.user) {
-      return response.status(401).send();
-    }
-    return response.json((request.user as IProfile).getSafeProfile());
-  }
-);
-
-router.patch(
-  "/",
-  authenticationRequired,
-  (request: Request, response: Response) => {
-    if (!request.user) {
-      return response.status(401).send();
-    }
-    const { email, firstname, lastname, password } = request.body;
-
-    updateProfile(
-      request.user as IProfile,
-      email,
-      firstname,
-      lastname,
-      password
-    )
-      .then((profile) => {
-        if (!profile) return response.status(404).send("Profile not found");
-        return response.status(200).send(profile.getSafeProfile());
-      })
-      .catch((error) => {
-        console.error(error);
-        return response.status(500).send();
-      });
-  }
-);
-
-router.delete("/", authenticationRequired, (req: Request, res: Response) => {
-  if (!request.user) {
+router.get("/me", authenticationRequired, (req: Request, res: Response) => {
+  if (!req.user) {
     return res.status(401).send();
   }
-  (request.user as IProfile)
+  return res.json((req.user as IProfile).getSafeProfile());
+});
+
+router.patch("/", authenticationRequired, (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).send();
+  }
+  const { email, firstname, lastname, password } = req.body;
+
+  updateProfile(req.user as IProfile, email, firstname, lastname, password)
+    .then((profile) => {
+      if (!profile) return res.status(404).send("Profile not found");
+      return res.status(200).send(profile.getSafeProfile());
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).send();
+    });
+});
+
+router.delete("/", authenticationRequired, (req: Request, res: Response) => {
+  if (!req.user) {
+    return res.status(401).send();
+  }
+  (req.user as IProfile)
     .deleteOne()
     .then((_profile) => res.status(200).send("Utilisateur supprimé"))
     .catch((error) => {
