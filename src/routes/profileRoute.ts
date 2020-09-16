9d824d1b756d7cb1008a806c4bbec02deadeb867
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-16 15:25:51 +0200

    Update profile route

--- src/routes/profileRoute.ts
+++ src/routes/profileRoute.ts
@@ -86,7 +86,7 @@ router.get(
   }
 );
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", authenticationRequired, (req: Request, res: Response) => {
   getAllProfiles()
     .then((profiles) => profiles.map((profile) => profile.getSafeProfile()))
     .then((safeProfiles) => {
