26e5f832b1c50df27381fbc202e76fa4b9ea1ea7
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-10-06 09:46:22 +0200

    Update server file with SameSite attribute

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import {
 import profileRoutes from "./routes/profileRoute";
 import loginRoute from "./routes/loginRoute";
 import messageRoute from "./routes/messageRoute";
-// import webrtcRoute from "./routes/webrtcRoute";
+// import webrtcRoute from './routes/webrtcRoute';
 
 import session from "express-session";
 import connectMongo from "connect-mongo";
@@ -39,15 +39,23 @@ export function createExpressApp(config: IConfig): express.Express {
     })
   );
 
-  app.use(
-    session({
-      name: session_cookie_name,
-      secret: session_secret,
-      resave: false,
-      saveUninitialized: false,
-      store: sessionStore,
-    })
-  );
+  const sessionConfig: session.SessionOptions = {
+    name: session_cookie_name,
+    secret: session_secret,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {},
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    app.set("trust proxy", 1); // trust first proxy
+    sessionConfig.cookie = {
+      secure: true,
+      sameSite: "none",
+    };
+  }
+  app.use(session(sessionConfig));
 
   app.use(authenticationInitialize());
   app.use(authenticationSession());
@@ -60,7 +68,7 @@ export function createExpressApp(config: IConfig): express.Express {
   app.use("/profile", profileRoutes);
   app.use("/login", loginRoute);
   app.use("/messages", messageRoute);
-  // app.use("/webrtc", webrtcRoute);
+  // app.use('/webrtc', webrtcRoute);
 
   app.get("/", (req: Request, res: Response) => {
     res.send("This is the boilerplate for Flint Messenger app");
